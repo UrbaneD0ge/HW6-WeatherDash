9baee2403aceccf0394f98e4d7e2db20331da058
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-28 22:38:14 -0400

    Geo call broken, out of time

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,46 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={cc173e9e542edac08053c19dd02b4516}
 const key = "cc173e9e542edac08053c19dd02b4516";
+var rUrl = "https://api.openweathermap.org/data/2.5/onecall?";
+var gUrl = "http://api.openweathermap.org/geo/1.0/direct?";
 var lat = 33.74;
 var long = -84.38;
-var part;
-var rUrl = "https://api.openweathermap.org/data/2.5/onecall?";
-var gUrl = "http://api.openweathermap.org/geo/1.0/direct?"
-var loch; 
-var q;
 var limit = 5;
+var part;
+var loch = "Atlanta, GA"
+var q;
 // var searchTarget = $('#letsGeo')
 
+// form handler
 $('#letsGeo').submit(function(event) {
     event.preventDefault;
-    console.log('I need sleep.');
+    localStorage.setItem("city", ($('#geo').val()));
     console.log($('#geo').val());
-    // window.alert('You need sleep!');
-    let loch = ($('#geo').val);
-    geo(loch);
-})
+    geo();
+});
 
-// function geo () {
-//     fetch (gUrl + 'q=' + q + '&limit=' + limit + '&appid=' + key )
-//         .then(function
-//             (response) {
-//                 if (response.ok) {
-//                     console.log(response);
-//                     response.json().then(function (data) {
-//                         console.log(data);
-//                     })
-//                 } else {alert('Error: ' + response.statusText);}
-//             })
-//             .catch(function (error) {
-//                 alert('Unable to get geo data from OpenWeather');
-//             });
-// };
+console.log(loch);
 
-function meteo(lat, long) { fetch (rUrl + 'lat=' + lat + '&lon=' + long + '&units=imperial' + '&appid=' + key )
-    debugger
+// GeoAPI call
+function geo () {
+    fetch (gUrl + 'q=' + loch + '&limit=' + limit + '&appid=' + key )
+    .then(function
+        (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    // meteo(response.lat,response.lon)
+                })
+            } else {alert('Error: ' + response.statusText);}
+        })
+        .catch(function (error) {
+                debugger;
+                alert('Unable to get geo data from OpenWeather');
+            });
+};
+
+// 'OneCall' Forecast API call
+function meteo() { fetch (rUrl + 'lat=' + lat + '&lon=' + long + '&units=imperial' + '&appid=' + key )
     then(function
         (response) {
             if (response.ok) {
@@ -44,6 +48,7 @@ function meteo(lat, long) { fetch (rUrl + 'lat=' + lat + '&lon=' + long + '&unit
                 response.json().then(function (data) {
                     console.log(data);
                     // displayFn(data,city);
+
                 })
             } else {
                 alert('Error: ' + response.statusText);
