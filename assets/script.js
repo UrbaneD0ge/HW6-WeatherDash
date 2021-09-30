a49b2f81b269eac3febc63b350235595e6df6561
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-29 20:28:01 -0400

    API Calls fixed

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var rUrl = "https://api.openweathermap.org/data/2.5/onecall?";
 var gUrl = "http://api.openweathermap.org/geo/1.0/direct?";
 var lat = 33.74;
 var long = -84.38;
-var limit = 5;
+var limit = 1;
 var part;
 var loch = "Atlanta, GA"
 var q;
@@ -12,42 +12,49 @@ var q;
 
 // form handler
 $('#letsGeo').submit(function(event) {
-    event.preventDefault;
-    localStorage.setItem("city", ($('#geo').val()));
+    event.preventDefault();
+    loch = $('#geo').val();
+    localStorage.setItem("city", loch);
     console.log($('#geo').val());
     geo();
 });
 
-console.log(loch);
+//console.log(loch);
 
 // GeoAPI call
 function geo () {
-    fetch (gUrl + 'q=' + loch + '&limit=' + limit + '&appid=' + key )
+    let request = gUrl + 'q=' + encodeURIComponent(loch) + '&limit=' + limit + '&appid=' + key;
+    console.log(request);
+    fetch (request)
+    .then(function
+        (response) {
+            console.log(response);
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log('return from API',data);
+                    [lat,long] = [data[0].lat,data[0].lon];
+                    meteo();
+                })
+            } else {alert('Error: ' + response.statusText);}
+        })
+    .catch(function (error) {
+            alert('Unable to get geo data from OpenWeather');
+        });
+};
+
+// 'OneCall' Forecast API call
+
+function meteo() {
+    let request = rUrl + 'lat=' + lat + '&lon=' + long + '&units=imperial' + '&appid=' + key;
+    fetch(request)
     .then(function
         (response) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (data) {
                     console.log(data);
-                    // meteo(response.lat,response.lon)
-                })
-            } else {alert('Error: ' + response.statusText);}
-        })
-        .catch(function (error) {
-                debugger;
-                alert('Unable to get geo data from OpenWeather');
-            });
-};
-
-// 'OneCall' Forecast API call
-function meteo() { fetch (rUrl + 'lat=' + lat + '&lon=' + long + '&units=imperial' + '&appid=' + key )
-    then(function
-        (response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
                     // displayFn(data,city);
+                    
 
                 })
             } else {
